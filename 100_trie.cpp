3a8c5f9226281fbbb4c3622cdd7055644830afad
Nikshay <nikshay2018@gmail.com>
2023-07-19 03:46:53 +0530

    Completed Heaps

---
+++ 100_trie.cpp
@@ -0,0 +1,148 @@
+// A Trie is used instead of map because space utilisation is better in Trie as compared to map. For instance, if we wish to create a dictionary consisting of words like ARE, ARM, ARMORY, ARMS, ARENA, etc, a separate block will be created in a map for all these words but in a Trie the story is different, as it will create separate block only when new letters are encountered.
+// Another advantage of Trie is that if we want to search words starting with AR, we will need very less comparisions than required in map.
+#include <bits/stdc++.h>
+using namespace std;
+
+class TrieNode
+{
+public:
+    char data;
+    TrieNode *children[26];
+    bool isTerminal;
+    TrieNode(char c)
+    {
+        data = c;
+        for (int i = 0; i < 26; i++)
+            children[i] = NULL;
+        isTerminal = false;
+    }
+}; 
+
+class Trie
+{
+public:
+    TrieNode *root;
+    Trie()
+    {
+        root = new TrieNode('\0');
+    }
+
+    // INSERTION FUNCTION
+    void insertUtil(TrieNode *root, string word)
+    {
+        // base case
+        if (word.length() == 0)
+        {
+            root->isTerminal = true;
+            return;
+        }
+        // assumption, word will be in CAPS
+        int index = word[0] - 'A';
+        TrieNode *child;
+
+        // present
+        if (root->children[index] != NULL)
+            child = root->children[index];
+        // absent
+        else
+        {
+            child = new TrieNode(word[0]);
+            root->children[index] = child;
+        }
+
+        // Recursive Call
+        insertUtil(child, word.substr(1));
+    }
+    void insertWord(string word)
+    {
+        insertUtil(root, word);
+    }
+
+    // SEARCH FUNCTION
+    bool searchUtil(TrieNode *root, string word)
+    {
+        if (word.length() == 0)
+            return root->isTerminal;
+        int index = word[0] - 'A';
+        TrieNode *child;
+        if (root->children[index] != NULL)
+            child = root->children[index];
+        else
+            return false;
+        return searchUtil(child, word.substr(1));
+    }
+    bool search(string word)
+    {
+        return searchUtil(root, word);
+    }
+
+    // REMOVAL FUNCTION
+    /*
+     During delete operation we delete the key in bottom up manner using  recursion. The following are possible conditions when deleting  keyfrom trie,
+     * Key may not be there in trie. Delete operation should not modify  trie.
+     * Key present as unique key (no part of key contains another key  (prefix), nor the key itself is prefix of another key in trie).  Delete all the nodes.
+     * Key is prefix key of another long key in trie. Unmark the leaf node.
+     * Key present in trie, having atleast one other key as prefix key. Delete nodes from end of key until first leaf node of longest prefix key.
+     */
+    // Returns true if the root has no child
+    bool isEmpty(TrieNode *root)
+    {
+        for (int i = 0; i < 26; i++)
+            if (root->children[i])
+                return false;
+        return true;
+    }
+
+    // Recursive function to delete a key from given Trie
+    TrieNode *remove(TrieNode *root, string key, int depth = 0)
+    {
+        // If tree is empty
+        if (!root) return NULL;
+
+        // If last character of key is being processed
+        if (depth == key.size())
+        {
+            // This node is no more end of word after removal of given key
+            if (root->isTerminal) root->isTerminal = false;
+
+            // If given is not prefix of any other word
+            if (isEmpty(root))
+            {
+                delete (root);
+                root = NULL;
+            }
+            return root;
+        }
+
+        // If not last character, recur for the child obtained using ASCII value
+        int index = key[depth] - 'A';
+        root->children[index] = remove(root->children[index], key, depth + 1);
+
+        // If root does not have any child (its only child got deleted), and it is not end of another word.
+        if (isEmpty(root) && root->isTerminal == false)
+        {
+            delete (root);
+            root = NULL;
+        }
+        return root;
+    }
+    void removeWord(string key){
+        remove(root, key);
+    }
+};
+int main()
+{
+    Trie *t = new Trie();
+
+    t->insertWord("ARM");
+    t->insertWord("TIME");
+    t->insertWord("DO");
+    t->insertWord("TIM");
+
+    cout << "Tim Present or Not " << t->search("TIM") << "\n";
+    cout << "Time Present or Not " << t->search("TIME") << "\n";
+    t->removeWord("TIM");
+    cout << "Tim Present or Not " << t->search("TIM") << "\n";
+    cout << "Time Present or Not " << t->search("TIME") << "\n";
+    return 0;
+}
---
+++ 101_LCP_and_phone_dir.cpp
@@ -0,0 +1,190 @@
+// LONGEST COMMON PREFIX
+// class TrieNode
+// {
+// public:
+// 	char data;
+// 	TrieNode *children[26];
+// 	TrieNode(char c)
+// 	{
+// 		data = c;
+// 		for (int i = 0; i < 26; i++)
+// 			children[i] = NULL;
+// 	}
+// };
+// class Trie
+// {
+// public:
+// 	TrieNode *root;
+// 	Trie()
+// 	{
+// 		root = new TrieNode('\0');
+// 	}
+// 	void insertUtil(TrieNode *root, string word)
+// 	{
+// 		if (word.length() == 0)
+// 			return;
+// 		int index = word[0] - 'a';
+// 		TrieNode *child;
+// 		if (root->children[index])
+// 			child = root->children[index];
+// 		else
+// 		{
+// 			child = new TrieNode(word[0]);
+// 			root->children[index] = child;
+// 		}
+// 		insertUtil(child, word.substr(1));
+// 	}
+// 	void insert(string word)
+// 	{
+// 		insertUtil(root, word);
+// 	}
+// 	int childCount(TrieNode *root)
+// 	{
+// 		if (!root)
+// 			return 0;
+// 		int count = 0;
+// 		for (int i = 0; i < 26; i++)
+// 		{
+// 			if (root->children[i])
+// 				count++;
+// 		}
+// 		return count;
+// 	}
+// 	void LCP(string first, string &ans)
+// 	{
+// 		TrieNode *temp = root;
+// 		for (int i = 0; i < first.length(); i++)
+// 		{
+// 			if (childCount(temp) == 1)
+// 			{
+// 				ans.push_back(first[i]);
+// 				temp = root->children[(int)(first[i] - 'a')];
+// 			}
+// 			else
+// 				break;
+// 		}
+// 	}
+// };
+// string longestCommonPrefix(vector<string> &strs)
+// {
+// 	Trie *t = new Trie();
+// 	for (int i = 0; i < strs.size(); i++)
+// 	{
+// 		t->insert(strs[i]);
+// 	}
+// 	int min = 201;
+// 	string mini;
+// 	for (auto i : strs)
+// 	{
+// 		if (i.length() < min)
+// 		{
+// 			mini = i;
+// 			min = i.length();
+// 		}
+// 	}
+// 	string ans = "";
+// 	t->LCP(mini, ans);
+// 	return ans;
+// }
+
+// IMPLEMENT A PHONE DIRECTORY
+#include <bits/stdc++.h>
+using namespace std;
+class TrieNode
+{
+public:
+    char data;
+    TrieNode *children[26];
+    bool isTerminal;
+    TrieNode(char c)
+    {
+        data = c;
+        for (int i = 0; i < 26; i++)
+            children[i] = NULL;
+        isTerminal = false;
+    }
+};
+
+class Trie
+{
+public:
+    TrieNode *root;
+    Trie()
+    {
+        root = new TrieNode('\0');
+    }
+    void insertUtil(TrieNode *root, string word)
+    {
+        if (word.length() == 0)
+        {
+            root->isTerminal = true;
+            return;
+        }
+        int index = word[0] - 'a';
+        TrieNode *child;
+
+        if (root->children[index] != NULL)
+            child = root->children[index];
+        else
+        {
+            child = new TrieNode(word[0]);
+            root->children[index] = child;
+        }
+
+        insertUtil(child, word.substr(1));
+    }
+    void insertWord(string word)
+    {
+        insertUtil(root, word);
+    }
+    void printSuggestions(TrieNode *curr, vector<string> &temp, string prefix)
+    {
+        if (curr->isTerminal)
+            temp.push_back(prefix);
+        for (char ch = 'a'; ch <= 'z'; ch++)
+        {
+            TrieNode *next = curr->children[ch - 'a'];
+            if (next)
+            {
+                prefix.push_back(ch);
+                printSuggestions(next, temp, prefix);
+                prefix.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> getSuggestions(string str)
+    {
+        TrieNode *prev = root;
+        vector<vector<string>> output;
+        string prefix = "";
+        for (int i = 0; i < str.length(); i++)
+        {
+            char lastch = str[i];
+            prefix.push_back(lastch);
+            TrieNode *curr = prev->children[lastch - 'a'];
+            if (!curr)
+                break;
+            vector<string> temp;
+            printSuggestions(curr, temp, prefix);
+            output.push_back(temp);
+            temp.clear();
+            prev = curr;
+        }
+        return output;
+    }
+};
+vector<vector<string>> phoneDirectory(vector<string> &contactList, string &queryStr)
+{
+    //    Write your code here.
+    Trie *t = new Trie();
+    for (int i = 0; i < contactList.size(); i++)
+        t->insertWord(contactList[i]);
+
+    return t->getSuggestions(queryStr);
+}
+
+int main()
+{
+
+    return 0;
+}
