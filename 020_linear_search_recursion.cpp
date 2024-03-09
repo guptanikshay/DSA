21ffdccb023f67759b94f0a7037f952223c17211
Nikshay <nikshay2018@gmail.com>
2024-03-09 14:15:15 +0530

    Add sorting algos

--- 020_linear_search_recursion.cpp
+++ 020_linear_search_recursion.cpp
@@ -2,17 +2,19 @@
 #include <iostream>
 using namespace std;
 
-int linearSearch(int * arr, int n, int key){
-    static int count=0;
-    if(n==0)
-    return -1;
-    if(arr[0]==key)
-    return count;
-    count++;
-    return linearSearch(arr+1, n-1, key);
+int linearSearch(int *arr, int n, int key)
+{
+    static int index = 0;
+    if (n == 0)
+        return -1;
+    if (arr[0] == key)
+        return index;
+    index++;
+    return linearSearch(arr + 1, n - 1, key);
 }
-int main(){
-    int arr[]={1, 2, 3, 4, 5};
-    cout<<"Element is present in the array at index "<<linearSearch(arr, 5, 8)<<"\n";
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    cout << "Element is present in the array at index " << linearSearch(arr, 5, 8) << "\n";
     return 0;
 }
--- new.cpp
+++ new.cpp
@@ -1,19 +1,10 @@
 #include <iostream>
 using namespace std;
-bool isPrime(int n)
-{
-	if (n <= 1)
-		return false;
-	for (int i = 2; i * i < n; i++)
-		if (n % i == 0)
-			return false;
-	return true;
-}
+
 int main()
 {
-	if (isPrime(231))
-		cout << "YES\n";
-	else
-		cout << "NO\n";
+	int a;
+	cin >> a;
+	cout << a;
 	return 0;
 }
