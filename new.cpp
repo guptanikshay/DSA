4b67612c5b9f747c0009280c1f7c981ce9df9262
Nikshay <nikshay2018@gmail.com>
2024-08-09 19:54:49 +0530

    "Some minor changes"

--- new.cpp
+++ new.cpp
@@ -3,18 +3,7 @@ using namespace std;
 
 int main()
 {
-	string s;
-	cin >> s;
-	int sz = s.length();
-	int ans = 0;
-	for (int i = 0; i < sz / 2; i++)
-	{
-		string p = s.substr(0, i), q = s.substr(i, i + sz / 2), r = s.substr(i + sz / 2, sz);
-		cout << p << " " << q << " " << r << "\n";
-		string pr = p.append(r);
-		if (q.compare(pr) == 0)
-			ans++;
-	}
-	cout << ans << "\n";
+	int n = floor(-4 / 3);
+	cout << n << "\n";
 	return 0;
 }
