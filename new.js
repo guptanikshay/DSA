9f9b6f6e236cad5e7b90597229102a1d129bad03
Nikshay <nikshay2018@gmail.com>
2024-10-22 10:55:48 +0530

    nothing much

---
+++ new.js
@@ -0,0 +1,5 @@
+if (name.length > 3) {
+  document.getElementById("nameError").innerText = "Naam wala randi rona";
+  document.getElementById("nameError").style.display = "block";
+  isValid = false;
+}
