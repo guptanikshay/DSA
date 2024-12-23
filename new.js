a78bd4904124b14f948b0b7fb67ca4801b94fac3
Nikshay <nikshay2018@gmail.com>
2024-12-24 02:31:20 +0530

    Studied Cycle sort

--- new.js
+++
@@ -1,5 +0,0 @@
-if (name.length > 3) {
-  document.getElementById("nameError").innerText = "Naam wala randi rona";
-  document.getElementById("nameError").style.display = "block";
-  isValid = false;
-}
